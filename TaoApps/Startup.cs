977f447c78e96d37b15c60fd306a26224795f1af
dpeeters <dpeeters@cavancoco.ie>
2016-02-15 09:54:25 +0000

    Setup TaoApps C Sharp MVC

---
+++ TaoApps/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TaoApps.Startup))]
+namespace TaoApps
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
